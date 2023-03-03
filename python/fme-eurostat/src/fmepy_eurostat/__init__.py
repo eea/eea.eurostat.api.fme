a2a8c0acd609539f15cd69f5f57e3a8601966a04
Peter Segerstedt <peter.segerstedt@sweco.se>
2023-03-03 16:18:54 +0100

    additional format for experimenting

--- python/fme-eurostat/src/fmepy_eurostat/__init__.py
+++ python/fme-eurostat/src/fmepy_eurostat/__init__.py
@@ -1,20 +1,27 @@
 from .reader import EurostatReader
-import logging
 from .constants import LOG_NAME
-log = logging.getLogger(LOG_NAME)
-log.info('Module loaded')
+from fmegeneral.fmelog import get_configured_logger
+from fmegeneral.plugins import FMEMappingFileWrapper
+from fmegeneral import fmeconstants
+
+READER_IMPL = {
+    'EEA.EUROSTAT.EUROSTATPY': EurostatReader
+}
 
 def FME_createReader(reader_type, src_keyword, mapping_file):
-    """
-    Reader entry point called by FME.
-    FME expects this function name and signature.
-    """
-    #from . import reader as reader_impl
-    log.warn('FME_createReader %s %s - Not implemented', reader_type, src_keyword)
-    return None #reader_impl.Reportnet3Reader(reader_type, src_keyword, mapping_file)
+    log = get_configured_logger(
+          src_keyword
+        , mapping_file.fetch(fmeconstants.kFME_DEBUG) is not None
+    )
+    if not reader_type in READER_IMPL:
+        log.warn('FME_createReader %s %s - Not implemented', reader_type, src_keyword)
+        return None
+    mapping_file_wrapper = FMEMappingFileWrapper(mapping_file, src_keyword, reader_type)
+    reader_cls = READER_IMPL.get(reader_type)
+    return reader_cls(mapping_file_wrapper, log)
 
 def FME_createWriter(writer_type, dest_keyword, mapping_file):
-    log.warn('FME_createWriter %s %s - Not implemented', writer_type, dest_keyword)
+    #log.warn('FME_createWriter %s %s - Not implemented', writer_type, dest_keyword)
     
     return None
     
--- python/fme-eurostat/src/fmepy_eurostat/reader.py
+++ python/fme-eurostat/src/fmepy_eurostat/reader.py
@@ -1,3 +1,61 @@
 from pluginbuilder import FMEReader
+from fmeobjects import FMEFeature
+
 class EurostatReader(FMEReader):
-    pass
+    def __init__(self, mapping_file, log):
+        self._log = log
+        self._log.info('EurostatReader initiated')
+        ''' mapping file wrapper methods:
+        defLines
+        fetchWithPrefix
+        get
+        getFeatureTypes
+        getFlag
+        getSearchEnvelope
+        mappingFile
+        '''
+        self._log.info('CONNECTION: %s', mapping_file.get('CONNECTION'))
+        self._log.info('DATAFLOW: %s', mapping_file.get('DATAFLOW'))
+        self._schema_iterator = iter([
+            ('Sample', [('asdf', 'str'),('bsdf', 'int')])
+        ])
+        
+    def abort(self):
+        pass
+    def close(self):
+        pass
+    def getProperties(self, propertyCategory):
+        self._log.debug('getProperties %s', propertyCategory)
+        return {
+                'fme_search_type': [
+                      'fme_search_type', 'fme_all_features'
+                ]
+                , 'fme_all_features': [
+                      'fme_all_features', 'fme_where'
+                    , 'fme_all_features', 'fme_feature_type'
+                ]
+            }.get(propertyCategory, None)
+    def open(self, datasetName, parameters):
+        self._log.info('open')
+        self._log.info(' datasetName: %s', datasetName)
+        self._log.info(' parameters: %s', parameters)
+    def read(self):
+        return None
+
+    def readSchema(self):
+        schema_record = next(self._schema_iterator, None)
+        self._log.info('schema record: %s', schema_record)
+        if schema_record:
+            feature_type, attributes = schema_record    
+            feature = FMEFeature()
+            feature.setFeatureType(feature_type)
+            for n,t in attributes:
+                feature.setSequencedAttribute(n, t)
+            feature.setAttribute('fme_geometry{}', ['eurostat_none'])
+            return feature
+        return None
+    def setConstraints(self, feature):
+        pass
+    def spatialEnabled(self):
+        return True
+    
