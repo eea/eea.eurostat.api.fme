d0e7965a53dc594b38c2a8e8b8c36f79f2e7492b
Peter Segerstedt <peter.segerstedt@sweco.se>
2023-03-14 13:13:19 +0100

    minor experiments

--- python/fme-eurostat/src/fmepy_eurostat/reader.py
+++ python/fme-eurostat/src/fmepy_eurostat/reader.py
@@ -1,5 +1,7 @@
 from pluginbuilder import FMEReader
 from fmeobjects import FMEFeature
+from fmegeneral.parsers import OpenParameters, parse_def_line
+from urllib.parse import (urlparse, parse_qs)
 
 class EurostatReader(FMEReader):
     def __init__(self, mapping_file, log):
@@ -15,10 +17,15 @@ class EurostatReader(FMEReader):
         mappingFile
         '''
         self._log.info('CONNECTION: %s', mapping_file.get('CONNECTION'))
-        self._log.info('DATAFLOW: %s', mapping_file.get('DATAFLOW'))
-        self._schema_iterator = iter([
-            ('Sample', [('asdf', 'str'),('bsdf', 'int')])
-        ])
+        self._log.info('START_PERIOD: %s', mapping_file.get('START_PERIOD'))
+
+
+        self._schema_iterator = None
+        self._feature_iterator = None
+        self._mapping_file = mapping_file
+
+        self._feature_types = set()
+
         
     def abort(self):
         pass
@@ -39,10 +46,48 @@ class EurostatReader(FMEReader):
         self._log.info('open')
         self._log.info(' datasetName: %s', datasetName)
         self._log.info(' parameters: %s', parameters)
+        parsed_parameters = OpenParameters(datasetName, parameters)
+        self._log.info(' parsed_parameters: %s', parsed_parameters)
+        mapping_file_parameters = {k: self._mapping_file.get(k) for k in ['CONNECTION', 'START_PERIOD', 'END_PERIOD', 'FIRST_N_OBSERVATIONS', 'LAST_N_OBSERVATIONS']}
+        self._log.info(' mapping_file_parameters: %s', mapping_file_parameters)
+
+        for feature_type_id in parsed_parameters.get('+ID', []):
+            self._log.info('  feature_type_id: %s', feature_type_id)
+            feature_type_url = urlparse(feature_type_id) # expecting fme://-url from web select here
+            #self._log.info('  feature_type_url: %s', feature_type_url)
+            query_params = parse_qs(feature_type_url.query)
+            #self._log.info('  query_params: %s', query_params)
+            dataflow_id, *_ = query_params.get('id', [None])
+            #self._log.info('  dataflow_id: %s', dataflow_id)
+            self._feature_types.add(dataflow_id)
+        self._log.info('Parsing mapping file def-lines')
+        for defline in self._mapping_file.defLines():
+            feature_type, attributes, options = parse_def_line(defline, ['fme_attribute_reading', 'eurostat_where_clause'])
+            self._log.info(' %s %s %s', feature_type, attributes, options)
+            self._feature_types.add(feature_type)
     def read(self):
+        if self._feature_iterator is None:
+            self._feature_iterator = iter([
+                (ft, [('asdf', 'Hello World!'),('bsdf', 42)])
+                for ft in self._feature_types
+            ])
+        record = next(self._feature_iterator, None)
+        if record:
+            feature_type, attributes = record    
+            feature = FMEFeature()
+            feature.setFeatureType(feature_type)
+            for n,t in attributes:
+                feature.setAttribute(n, t)
+            return feature
         return None
 
+
     def readSchema(self):
+        if self._schema_iterator is None:
+            self._schema_iterator = iter([
+                (ft, [('asdf', 'str'),('bsdf', 'int')])
+                for ft in self._feature_types
+            ])
         schema_record = next(self._schema_iterator, None)
         self._log.info('schema record: %s', schema_record)
         if schema_record:
